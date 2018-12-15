ebe86e8711f95b16195638013b2eaa1bd1b2e951
harryariyanto <m26415133@john.petra.ac.id>
2018-12-15 17:00:56 +0100

    moviesClassCanBeMocked

--- src/test/java/WCAControllerTest.java
+++ src/test/java/WCAControllerTest.java
@@ -1,3 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class WCAControllerTest {
     WCAController controller;
+
+    @Test
+    public void moviesClassCanBeMocked(){
+        Movies movies=mock(Movies.class);
+        Assert.assertTrue(movies instanceof Movies);
+    }
 }
