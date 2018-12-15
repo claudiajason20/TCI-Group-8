62a8f639492867978007377bce761692c8e0c5e1
harryariyanto <m26415133@john.petra.ac.id>
2018-12-15 14:39:49 +0100

    constructorSuccessful RED

---
+++ src/test/java/moviesTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class moviesTest {
+    @Test
+    public void constructorSuccessful(){
+        //arrange
+        int ID=1;
+        String NAME="Hi BEN";
+        String GENRE="Drama";
+        String FORMAT="DVD";
+        String YEAR="1997";
+        String DIRECTOR="Jack";
+        String WRITER="Jok";
+        String STAR="star";
+        //act
+        Movies movies=new Movies(ID,NAME,GENRE,FORMAT,YEAR,DIRECTOR,WRITER,STAR);
+        //assert
+        Assert.assertEquals(ID,movies.getid());
+        Assert.assertEquals(NAME,movies.getname());
+        Assert.assertEquals(GENRE,movies.getgenre());
+        Assert.assertEquals(FORMAT,movies.getformat());
+        Assert.assertEquals(YEAR,movies.getyear());
+        Assert.assertEquals(DIRECTOR,movies.getdirector());
+        Assert.assertEquals(WRITER,movies.getwriter());
+        Assert.assertEquals(STAR,movies.getstar());
+
+
+    }
+}
