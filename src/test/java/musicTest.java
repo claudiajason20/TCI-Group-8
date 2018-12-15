92d30c09ab7411242ad32dbca60b3085b46b4b5c
Andy <m26415147@john.petra.ac.id>
2018-12-15 15:01:42 +0100

    finished refactoring for musicYearDoesNotComeFromFututreAssertion by adding javadoc

--- src/test/java/musicTest.java
+++ src/test/java/musicTest.java
@@ -4,8 +4,16 @@ import java.util.Date;
 
 import static org.junit.Assert.*;
 
+/**
+ *
+ */
 public class musicTest {
 
+    /**
+     * This test asserts whether the year of the released music is valid or not, music are not supposed to be recorded from the future
+     *
+     * @throws musicYearException
+     */
     @Test(expected = musicYearException.class)
     public void musicYearDoesNotComeFromFutureAssertion() throws musicYearException {
         //arrange
@@ -21,6 +29,5 @@ public class musicTest {
         //act
         Music musicTest=new Music(id,Title,Category,Genre,Format,Year,Artist);
         //assert is handled by the exception
-
     }
-}
+  }
