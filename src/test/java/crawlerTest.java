d7069305a7dc718a198b29659461b19655bf16c0
Andy <m26415147@john.petra.ac.id>
2018-12-15 20:27:51 +0100

    verify that url method runs test created

--- src/test/java/crawlerTest.java
+++ src/test/java/crawlerTest.java
@@ -1,18 +1,30 @@
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
 
 public class crawlerTest {
     Crawler crawler=new Crawler();
+
+    /**
+     * THis test verifies whetehr the verifyUrl returns the correct value
+     */
     @Test
     public void verifyThatUrlIsValidBeforeAddedToUrlQueue(){
         //arrange
-        String url="https://www.geeksforgeeks.org/check-if-url-is-valid-or-not-in-java/";
         boolean validUrl;
+        String url="https://www.geeksforgeeks.org/check-if-url-is-valid-or-not-in-java/";
         //act
         validUrl=crawler.verifyUrl(url);
         //assertion is done by the exception
         assertTrue(validUrl);
+    }
+    @Test
+    public void verifyThatVerifyUrlMethodRuns(){
+        String url="https://www.geeksforgeeks.org/check-if-url-is-valid-or-not-in-java/";
 
+        Crawler spyCrawl= Mockito.spy(crawler);
+        verify(spyCrawl).verifyUrl(url);
     }
 }
