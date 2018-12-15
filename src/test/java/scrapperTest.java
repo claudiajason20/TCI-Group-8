4c9f88c4df91ffed029725ab2094ffb5dc034058
Claudia Nathasia Jason,Claudia C <c.jason@student.fontys.nl>
2018-12-15 17:48:14 +0100

    Book Class is not empty -Red

---
+++ src/test/java/scrapperTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class scrapperTest {
+    @Test
+    public void bookIsNotNull() {
+        //arrange
+        Books book = null;
+        //act
+        //assert
+        assertTrue(book!=null);
+    }
+}
