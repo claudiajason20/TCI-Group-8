ab5d630810758ddf0dd78743f52b83f77e5d61d3
Claudia Nathasia Jason,Claudia C <c.jason@student.fontys.nl>
2018-12-15 14:39:22 +0100

    Constructor Test -Red

--- src/main/java/Books.java
+++ src/main/java/Books.java
@@ -8,4 +8,31 @@ class Books {
         String cat = "meow";
         return cat;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFormat() {
+        return  format;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getIsbn() {
+    }
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -3,15 +3,39 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BookTest {
+
     @Test
-    public void BookIsEmpty() {
+    public void bookIsEmpty() {
+        //arrange
         Books book = new Books();
+        //assert
         assertEquals(0, book.numberOfData());
     }
 
     @Test
     public void checkString() {
+        //arrange
         Books book = new Books();
+        //act
+        //assert
         assertEquals("meow", book.getTitle());
     }
+
+    @Test
+    public void constructorIsNotNull(){
+        //arrange
+        Books book = new Books();
+        //act
+        //assert
+
+        assertEquals(1,book.getId());
+        assertEquals("Audio",book.getFormat());
+        assertEquals("2011", book.getYear());
+        assertEquals("Robert C. Martin", book.getAuthors());
+        assertEquals("Prentice Hall", book.getPublisher());
+        assertEquals("Tech",book.getGenre());
+        assertEquals("007-6092046981", book.getIsbn());
+    }
+
+
 }
