b16a0c5e95373ab6dee5631069a9d35e844b862b
Claudia Nathasia Jason,Claudia C <c.jason@student.fontys.nl>
2018-12-16 20:01:02 +0100

    scrappert Test -Red

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -1,9 +1,18 @@
+import org.jsoup.nodes.Document;
 
 public class Scrapper {
+    private static Document docu;
 
-    /**
-     * @param book
-     */
-    public void decodeBook(Books book) {
+    public static void setDoc(Document doc) {
+        docu = doc;
+    }
+
+    public boolean text(Document doc) {
+//        return doc.body().hasText();
+        return true;
+    }
+
+    public String getBodyText() {
+        return docu.body().text();
     }
 }
--- src/test/java/scrapperTest.java
+++ src/test/java/scrapperTest.java
@@ -1,19 +1,31 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class scrapperTest {
     @Test
-    public void bookIsNotEmpty() {
+    public void documentIsNotEmpty() {
         //arrange
-        Books book = mock(Books.class);
+        Jsoup jsoup = mock(Jsoup.class);
+        String html = "Hello World";
+        Document doc = mock(Document.class);
         Scrapper scrapperData = new Scrapper();
+
         //act
-        when(book.getYear()).thenReturn("1987");
+        when(jsoup.).thenReturn("Hello World");
+        Scrapper.setDoc(jsoup.parse(html));
         //assert
-        scrapperData.decodeBook(book);
+        verify(scrapperData).getBodyText();
     }
+
 }
