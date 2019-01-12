af7958bd250e5a32e629f712b7c84330cbe57b37
harryariyanto <m26415133@john.petra.ac.id>
2019-01-12 11:02:52 +0100

    canInsertAndAccessMovies REFACTOR

--- src/test/java/WCAControllerTest.java
+++ src/test/java/WCAControllerTest.java
@@ -10,11 +10,16 @@ import static org.mockito.Mockito.*;
 public class WCAControllerTest {
 @Test
     public void canInsertAndAccessMovies(){
+//    dummy
+//    Can insert and access movies data
+//    Arrange
     WCAController wcaController=new WCAController();
     Movies movie=mock(Movies.class);
+//    Act
     wcaController.insertMovie(movie);
     ArrayList<Movies> resultList=wcaController.getMovies();
     Movies result= resultList.get(0);
+//    Assert
     Assert.assertEquals(result,movie);
 
 }
