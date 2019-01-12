f14d77397124b88c584b895c2e6d7dca30aff43c
Claudia Nathasia Jason,Claudia C <c.jason@student.fontys.nl>
2019-01-12 16:02:43 +0100

    Rebuild scaper

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -5,41 +5,4 @@ import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class Scrapper {
-    private static Document docu;
-
-    public static void setDoc(Document doc) {
-        docu = doc;
-    }
-
-    public boolean text(Document doc) {
-//        return doc.body().hasText();
-        return true;
-    }
-
-    public String getBodyText() {
-        return docu.body().text();
-    }
-
-    public Boolean hasVisited(Node node, Node node2) {
-        return node.hasSameValue(node2);
-    }
-
-    /*
-     * to check the document that was sent is not empty.
-     */
-    public Boolean checkElement(Element doc) {
-        return doc.hasText();
-    }
-
-    /*
-     * Will return String of the data based on the Id
-     */
-    public String getDataById(int i) {
-        return "music";
-    }
-
-
-    public Object getSpecific(int i) {
-        return "music";
-    }
 }
