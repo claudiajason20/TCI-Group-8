956129163a85bc973b66b91c8b658901be3053e0
harryariyanto <m26415133@john.petra.ac.id>
2019-01-12 10:39:35 +0100

    WCACONTROLLER REMOVE NOT USED TEST and FUNCTION

--- src/main/java/WCAController.java
+++ src/main/java/WCAController.java
@@ -1,29 +1,5 @@
 public class WCAController {
 
-    public void insertmovies(Movies movies) {
-
-    }
-
-    public Movies getmovies() {
-        return null;
-    }
-
-    public Books getbooks() {
-        return null;
-    }
-
-    public void insertbooks(Books books) {
-
-    }
-
-    public Music getmusics() {
-        return null;
-    }
-
-    public void insertmusic(Music music) {
-
-    }
-
 
     public String getAll(String baseaddress) {
 
